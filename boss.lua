c29db1fa69546eb76a096388558be206dc68966b
Golgrax <70707230+Golgrax@users.noreply.github.com>
2023-06-20 10:01:19 +0800

    Update boss.lua

--- boss.lua
+++ boss.lua
@@ -1,21 +1,19 @@
--- Teleport function
-local function teleportToPlace()
-    local placeId = 1930863474
-    game:GetService("TeleportService"):Teleport(placeId)
+-- Create a ScreenGui
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+-- Create a TextButton
+local button = Instance.new("TextButton")
+button.Name = "TeleportButton"
+button.Text = "Teleport"
+button.Size = UDim2.new(0, 200, 0, 50)
+button.Position = UDim2.new(0.5, -100, 0.5, -25)
+button.Parent = gui
+
+-- Function to handle button click
+local function teleportPlayer()
+    game:GetService("TeleportService"):Teleport(1930863474)
 end
 
--- Create a button
-local button = Instance.new("TextButton")
-button.Size = UDim2.new(0, 100, 0, 50)
-button.Position = UDim2.new(0.5, -50, 0.5, -25)
-button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-button.BorderColor3 = Color3.fromRGB(0, 0, 0)
-button.TextColor3 = Color3.fromRGB(0, 0, 0)
-button.Text = "Golgrax"
-button.Font = Enum.Font.SourceSans
-
--- Parent the button to the game's core GUI
-button.Parent = game:GetService("CoreGui")
-
--- Connect the teleport function to the button's Activated event
-button.Activated:Connect(teleportToPlace)
+-- Connect the button click event to the teleportPlayer function
+button.MouseButton1Click:Connect(teleportPlayer)
